ca8611639ef76477af1f0d37d17747356a9980a9
Sn4k3rx <ramonvieira122333@gmail.com>
2020-02-05 09:17:09 -0300

    Excessões e limpando a tela

--- src/application/UI.java
+++ src/application/UI.java
@@ -29,7 +29,13 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
-	
+
+	// https://stackoverflow.com/questions/2979383/java-clear-the-console
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+
 	public static ChessPosition readChessPosition(Scanner in) {
 		try {
 			String s = in.nextLine();
@@ -39,9 +45,9 @@ public class UI {
 		} catch (RuntimeException e) {
 			throw new InputMismatchException("Erro na posição do xadrez. Valores validos de a1 até h8");
 		}
-		
+
 	}
-	
+
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print(8 - i + " ");
