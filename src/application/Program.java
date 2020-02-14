e416882dee3e9eac296d82538b60f199cf6a2315
Sn4k3rx <ramonvieira122333@gmail.com>
2020-02-14 15:46:26 -0300

    Movimento de promoção

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,6 +40,11 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Digite a peca para a promocao: (B/N/R/Q):  ");
+					String type = in.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				in.nextLine();
